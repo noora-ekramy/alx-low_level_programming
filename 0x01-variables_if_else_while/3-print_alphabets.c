fbecb6128b890437002108d958004b84f11d6c45
Nora Ekramy <nora.ekramy11@gmail.com>
2023-03-18 04:45:56 +0200

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,19 +4,19 @@ int main() {
     char lowercase_letter = 'a';
     char uppercase_letter = 'A';
 
-    // Print the lowercase alphabet
+   
     while (lowercase_letter <= 'z') {
         putchar(lowercase_letter);
         lowercase_letter++;
     }
     
-    // Print the uppercase alphabet
+    
     while (uppercase_letter <= 'Z') {
         putchar(uppercase_letter);
         uppercase_letter++;
     }
     
-    // Print a newline character
+    
     putchar('\n');
 
     return 0;
