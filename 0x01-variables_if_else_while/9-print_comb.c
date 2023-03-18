6ae93dc802151d2a57d8452e2577eddc2c4df906
Nora Ekramy <nora.ekramy11@gmail.com>
2023-03-18 04:50:57 +0200

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,18 +2,19 @@
 
 int main() {
     int i, j;
-    
+
     for (i = 0; i < 10; i++) {
         for (j = i; j < 10; j++) {
-            putchar(i + '0');
-            putchar(',');
-            putchar(' ');
-            putchar(j + '0');
-            putchar(' ');
+            putchar((i * 10 + j) / 10 + '0');
+            putchar((i * 10 + j) % 10 + '0');
+            if (i != 9 || j != 9) {
+                putchar(',');
+                putchar(' ');
+            }
         }
     }
-    
+
     putchar('\n');
-    
+
     return 0;
 }
