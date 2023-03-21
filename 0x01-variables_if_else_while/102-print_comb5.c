55960db21eea2db704774058fcc45a1e269ce120
Nora Ekramy <nora.ekramy11@gmail.com>
2023-03-21 04:11:45 +0200

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,11 +16,11 @@ int num1 = i / 10;
 int num2 = i % 10;
 int num3 = j / 10;
 int num4 = j % 10;
-putchar(num1 + '0');
-putchar(num2 + '0');
+putchar(num1 ? num1 + '0' : '0');
+putchar(num2 ? num2 + '0' : '0');
 putchar(' ');
-putchar(num3 + '0');
-putchar(num4 + '0');
+putchar(num3 ? num3 + '0' : '0');
+putchar(num4 ? num4 + '0' : '0');
 if (i != 99 || j != 99)
 {
 putchar(',');
