36387c96449b7f3d15b3704c2f4e604134473209
Nora Ekramy <nora.ekramy11@gmail.com>
2023-03-21 03:21:34 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,10 @@
-5-printf.c
+#include <stdio.h>
+/**
+ * main - Makes the sum of two numbers
+ *
+ * Return: 0
+ */
+int main() {
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
