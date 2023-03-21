9b6296a7824d1c9c3e2fccdee37670f5af83c091
Nora Ekramy <nora.ekramy11@gmail.com>
2023-03-21 03:55:46 +0200

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+/**
+ * main - Makes the sum of two numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char letter = 'z';
 
-int main() {
-  char letter = 'z';
-
-  while (letter >= 'a') {
-    putchar(letter);
-    letter--;
-  }
-
-  putchar('\n');
-
-  return 0;
+while (letter >= 'a')
+{
+putchar(letter);
+letter--;
+}
+putchar('\n');
+return (0);
 }
