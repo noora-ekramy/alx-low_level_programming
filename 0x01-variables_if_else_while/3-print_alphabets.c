6eb19fa73f06fb2b6e3ed276682395b99c6d0f3c
Nora Ekramy <nora.ekramy11@gmail.com>
2023-03-21 03:49:39 +0200

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
+/**
+ * main - Makes the sum of two numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char lowercase_letter = 'a';
+char uppercase_letter = 'A';
 
-int main() {
-    char lowercase_letter = 'a';
-    char uppercase_letter = 'A';
-
-   
-    while (lowercase_letter <= 'z') {
-        putchar(lowercase_letter);
-        lowercase_letter++;
-    }
-    
-    
-    while (uppercase_letter <= 'Z') {
-        putchar(uppercase_letter);
-        uppercase_letter++;
-    }
-    
-    
-    putchar('\n');
-
-    return 0;
+while (lowercase_letter <= 'z')
+{
+putchar(lowercase_letter);
+lowercase_letter++;
+} 
+while (uppercase_letter <= 'Z')
+{
+putchar(uppercase_letter);
+uppercase_letter++;
+}
+putchar('\n');
+return (0);
 }
