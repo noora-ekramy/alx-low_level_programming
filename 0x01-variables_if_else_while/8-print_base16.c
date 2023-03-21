39b73a2ae3fc8ed1775a8a4fbef1c3a2eec2d224
Nora Ekramy <nora.ekramy11@gmail.com>
2023-03-21 03:57:39 +0200

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
+/**
+ * main - Makes the sum of two numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char digit = '0';
 
-int main() {
-    char digit = '0';
-    
-    while (digit <= '9') {
-        putchar(digit);
-        digit++;
-    }
-    
-    digit = 'a';
-    
-    while (digit <= 'f') {
-        putchar(digit);
-        digit++;
-    }
-    
-    putchar('\n');
-    
-    return 0;
+while (digit <= '9')
+{
+putchar(digit);
+digit++;
+}
+digit = 'a';
+while (digit <= 'f')
+{
+putchar(digit);
+digit++;
+}
+putchar('\n');
+return (0);
 }
