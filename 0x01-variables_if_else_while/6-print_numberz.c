fd1fa0bd5fac92afe79d2d5a9110fb8bd72b15d6
Nora Ekramy <nora.ekramy11@gmail.com>
2023-03-21 03:55:08 +0200

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
-
-int main() {
-    int i = 0;
-    
-    while (i < 10) {
-        putchar(i + '0');
-        i++;
-    }
-    
-    putchar('\n');
-    
-    return 0;
+/**
+ * main - Makes the sum of two numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int i = 0;
+ 
+while (i < 10)
+{
+putchar(i + '0');
+i++;
+}
+putchar('\n');
+return (0);
 }
