964f185bb0bf2b1d17d3ff8743032f724186eec3
Nora Ekramy <nora.ekramy11@gmail.com>
2023-03-21 03:52:01 +0200

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
-
-int main() {
-    char letter = 'a';
+/**
+ * main - Makes the sum of two numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char letter = 'a';
     
-    while (letter <= 'z') {
-        if (letter != 'e' && letter != 'q') {
-            putchar(letter);
-        }
-        letter++;
-    }
-    
-    putchar('\n');
-    
-    return 0;
+while (letter <= 'z')
+{
+if (letter != 'e' && letter != 'q')
+{
+putchar(letter);
+}
+letter++;
+}  
+putchar('\n');
+return (0);
 }
