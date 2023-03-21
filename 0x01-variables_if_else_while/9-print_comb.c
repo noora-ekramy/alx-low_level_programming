d1810bc97bfd52d0c30a7fcfcb854e81ac63f257
Nora Ekramy <nora.ekramy11@gmail.com>
2023-03-21 03:58:39 +0200

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
+/**
+ * main - Makes the sum of two numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int i, j;
 
-int main() {
-    int i, j;
-
-    for (i = 0; i < 10; i++) {
-        for (j = i; j < 10; j++) {
-            putchar((i * 10 + j) / 10 + '0');
-            putchar((i * 10 + j) % 10 + '0');
-            if (i != 9 || j != 9) {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-
-    putchar('\n');
-
-    return 0;
+for (i = 0; i < 10; i++)
+{
+for (j = i; j < 10; j++)
+{
+putchar((i * 10 + j) / 10 + '0');
+putchar((i * 10 + j) % 10 + '0');
+if (i != 9 || j != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }
