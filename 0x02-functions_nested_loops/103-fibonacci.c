644bdb79f76da17b9afa5467b0b677d51ce3083d
Nora Ekramy <nora.ekramy11@gmail.com>
2023-03-22 03:52:02 +0200

    Create 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+/**
+ * Calculates the sum of even-valued terms in the Fibonacci sequence whose
+ * values do not exceed 4,000,000.
+ *
+ * Returns the sum.
+ */
+#include <stdio.h>
+
+int main()
+{
+    int current = 2;
+    int previous = 1;
+    int sum = 0;
+
+    while (current < 4000000) {
+        if (current % 2 == 0) {
+            sum += current;
+        }
+        int temp = current;
+        current += previous;
+        previous = temp;
+    }
+
+    printf("%d\n", sum);
+    return 0;
+}
