853318d73af495ac56857aef2372a9d0e1bb0bbb
Nora Ekramy <nora.ekramy11@gmail.com>
2023-03-22 04:12:34 +0200

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,16 +13,16 @@ int i;
 long a = 1, b = 2;
 long next;
 printf("%ld", a);
-printf(", ");   
+printf(", ");
 printf("%ld", b);
-printf(", ");       
-for (i = 1; i <= 98-2; i++)
+printf(", ");
+for (i = 1; i <= 98 - 2; i++)
 {
 next = a;
 a = b;
 b = next + b;
 printf("%ld", b);
-if (i != 98-1) 
+if (i != 98 - 2) 
 {
 printf(", ");
 } else {
