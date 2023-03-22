a66d241a2d4ed8ebaa6d87e50fd4e1faa2af2802
Nora Ekramy <nora.ekramy11@gmail.com>
2023-03-22 02:09:15 +0200

    Create main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* putchar - writes a character to stdout */
+int _putchar(char c);
+
+/* main - the main function of the program */
+int main(void);
+
+#endif /* MAIN_H */
