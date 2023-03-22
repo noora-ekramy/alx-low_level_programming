19dae52765c5cf980a4188ea62bf1d3f54bcc44c
Nora Ekramy <nora.ekramy11@gmail.com>
2023-03-22 02:28:22 +0200

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,6 +4,21 @@
 /* putchar - writes a character to stdout */
 int _putchar(char c);
 
+/* print_alphabet_x10 - func*/
+void print_alphabet_x10(void);
+
+/* _islower - func*/
+int _islower(int c);
+
+/* _isalpha - func*/
+int _isalpha(int c);
+
+/* print_sign - func*/
+int print_sign(int n);
+
+/* _abs - func*/
+int _abs(int);
+
 /* main - the main function of the program */
 int main(void);
 
