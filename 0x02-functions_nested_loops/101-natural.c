285faf720572980adf9cac63e0fad27ce27c30fb
Nora Ekramy <nora.ekramy11@gmail.com>
2023-03-22 03:38:16 +0200

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,7 +3,8 @@
 /**
  * main - Entry point
  *
- * Description: Computes and prints the sum of all multiples of 3 or 5 below 1024 (excluded)
+ * Description: Computes and prints the sum of
+ * all multiples of 3 or 5 below 1024 (excluded)
  *
  * Return: Always 0 (Success)
  */
