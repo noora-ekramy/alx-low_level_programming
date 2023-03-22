541e029761ad101f07655dea9958215204fab9cd
Nora Ekramy <nora.ekramy11@gmail.com>
2023-03-22 02:50:50 +0200

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -22,6 +22,24 @@ int print_sign(int n);
 /* _abs - func*/
 int _abs(int);
 
+/* print_last_digit - func*/
+int print_last_digit(int)
+  
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+void natural(void);
+
+
+
+
 /* main - the main function of the program */
 int main(void);
 
