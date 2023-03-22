76e59e28a868672a83fdb5b310b75e227c358bd2
Nora Ekramy <nora.ekramy11@gmail.com>
2023-03-22 04:03:09 +0200

    Create 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - function of the program.
+ * This function is the entry point of the program and is called by the
+ * system when the program is executed. It prints the "_putchar" string
+ * by a new line and returns 0 to indicate successful execution.
+ *
+ * Return:  0 to indicate successful execution.
+ */
+int main(void)
+{
+int i;
+int a = 1, b = 2;
+int next;
+for (i = 1; i <= 98; i++) {
+a = 1;
+b = 2;
+next = a;
+a = b;
+b = next + b;
+printf("%d", next());
+if (i != 98) {
+printf(", ");
+} else {
+printf("\n");
+}
+}
+    return (0);
+}
