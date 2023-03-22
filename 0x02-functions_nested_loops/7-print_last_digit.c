c13d1c41ac1556944f597ed4d0a802c554a1a473
Nora Ekramy <nora.ekramy11@gmail.com>
2023-03-22 03:04:02 +0200

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,9 +12,9 @@ int last_digit;
 
 if (n < 0)
 n = -n;
+last_digit = n % 10;
 if (last_digit < 0)
 last_digit = -last_digit;
-last_digit = n % 10;
 _putchar(last_digit + '0');
 
 return (last_digit);
