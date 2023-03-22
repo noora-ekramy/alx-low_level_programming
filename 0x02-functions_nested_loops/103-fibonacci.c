cb06bac5bd621267872725e3029a5262e743a511
Nora Ekramy <nora.ekramy11@gmail.com>
2023-03-22 03:53:03 +0200

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,5 +1,6 @@
 /**
- * Calculates the sum of even-valued terms in the Fibonacci sequence whose
+ * Calculates the sum of even-valued terms i
+ * the Fibonacci sequence whose
  * values do not exceed 4,000,000.
  *
  * Returns the sum.
@@ -8,19 +9,20 @@
 
 int main()
 {
-    int current = 2;
-    int previous = 1;
-    int sum = 0;
+int current = 2;
+int previous = 1;
+int sum = 0;
 
-    while (current < 4000000) {
-        if (current % 2 == 0) {
-            sum += current;
-        }
-        int temp = current;
-        current += previous;
-        previous = temp;
-    }
-
-    printf("%d\n", sum);
-    return 0;
+while (current < 4000000)
+{
+if (current % 2 == 0)
+{
+sum += current;
+}
+int temp = current;
+current += previous;
+previous = temp;
+}
+printf("%d\n", sum);
+return (0);
 }
