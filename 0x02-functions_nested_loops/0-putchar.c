8f653b17175df0d19f422e2afc184f59dcf8419f
Nora Ekramy <nora.ekramy11@gmail.com>
2023-03-22 02:14:11 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,6 @@
 #include"main.h"
 /**
  * main - function of the program.
- *
  * This function is the entry point of the program and is called by the 
  * system when the program is executed. It prints the "_putchar" string
  * by a new line and returns 0 to indicate successful execution.
@@ -11,6 +10,6 @@
  */
 int main(void)
 {
-   printf("_putchar\n");
-   return (0);
+printf("_putchar\n");
+return (0);
 }
