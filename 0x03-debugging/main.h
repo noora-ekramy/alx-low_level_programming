df165323b091f16a6fb7736ffc192b6118950c3b
Nora Ekramy <nora.ekramy11@gmail.com>
2023-03-24 04:28:05 +0200

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,6 +5,8 @@ void positive_or_negative(int i);
 
 int largest_number(int a, int b, int c);
 
+void print_remaining_days(int month, int day, int year);
+
 /* main - the main function of the program */
 int main(void);
 
