904a7a3851f8ea9e8a9175bc1db8b474e7518e11
Nora Ekramy <nora.ekramy11@gmail.com>
2023-03-31 01:06:13 +0200

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,21 +6,21 @@
  */
 void print_number(int n)
 {
-int power, digit, is_negative;
+    int power, digit, is_negative;
 
-power = 1;
-is_negative = n < 0;
-if (is_negative)
-_putchar('-');
-while (n / power > 9 || n / power < -9)
-power *= 10;
-while (power > 0)
-{
-digit = (n / power) % 10;
-if (is_negative)
-_putchar(-digit + '0');
-else
-_putchar(digit + '0');
-power /= 10;
-}
+    power = 1;
+    is_negative = n < 0;
+    if (is_negative)
+        _putchar('-');
+    while (n / power > 9 || n / power < -9)
+        power *= 10;
+    while (power > 0)
+    {
+        digit = (n / power) % 10;
+        if (is_negative)
+            _putchar(-digit + '0');
+        else
+            _putchar(digit + '0');
+        power /= 10;
+    }
 }
