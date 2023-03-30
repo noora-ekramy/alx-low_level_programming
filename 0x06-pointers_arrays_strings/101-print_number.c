2474b0b4521c9ce0505448ffab21d4b2f75dd560
Nora Ekramy <nora.ekramy11@gmail.com>
2023-03-31 01:05:25 +0200

    Create 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to print
+ */
+void print_number(int n)
+{
+int power, digit, is_negative;
+
+power = 1;
+is_negative = n < 0;
+if (is_negative)
+_putchar('-');
+while (n / power > 9 || n / power < -9)
+power *= 10;
+while (power > 0)
+{
+digit = (n / power) % 10;
+if (is_negative)
+_putchar(-digit + '0');
+else
+_putchar(digit + '0');
+power /= 10;
+}
+}
