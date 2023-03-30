dc595720585f066cb300123eb3490142f250f140
Nora Ekramy <nora.ekramy11@gmail.com>
2023-03-31 00:31:52 +0200

    Create main.h

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* putchar - writes a character to stdout */
+char *_strcat(char *dest, char *src);
+
+
+/* main - the main function of the program */
+int main(void);
+
+#endif /* MAIN_H */
