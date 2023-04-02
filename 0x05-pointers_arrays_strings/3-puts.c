f82c43f50d1823f19d6f8317102b153ce95c9b95
Nora Ekramy <nora.ekramy11@gmail.com>
2023-04-02 23:57:11 +0200

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,13 +9,13 @@
  */
 void _puts(char *str)
 {
-    int i = 0;
+int i = 0;
 
-    while (str[i] != '\0')
-    {
-        write(1, &str[i], 1);
-        i++;
-    }
+while (str[i] != '\0')
+{
+write(1, &str[i], 1);
+i++;
+}
 
-    write(1, "\n", 1);
+write(1, "\n", 1);
 }
