abcb6678aa71a48daf452ed255f5e1bccfe76888
Nora Ekramy <nora.ekramy11@gmail.com>
2023-04-04 03:25:29 +0200

    Create 100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - Sets the value of
+ * @s: Pointer to pointer to a
+ * @to: Pointer to a char
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
