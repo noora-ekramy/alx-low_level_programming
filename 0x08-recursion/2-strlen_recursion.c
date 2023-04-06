4f9ae238ef03f672d9e631d895535c0963dba5bc
Nora Ekramy <nora.ekramy11@gmail.com>
2023-04-06 04:07:01 +0200

    Create 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen_recursion
+ * @s: string to be measured
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+return (0);
+return (1 + _strlen_recursion(s + 1));
+}
