56f6aa240d2b756f93a35de461e7bba523ee359e
Nora Ekramy <nora.ekramy11@gmail.com>
2023-04-06 04:18:06 +0200

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * _print_rev_recursion
  * @s: Pointer to the
- *
  * Return: void
  */
 void _print_rev_recursion(char *s)
 {
 if (*s)
 {
-_print_rev_recursion(s + 1); 
+_print_rev_recursion(s + 1);
 _putchar(*s);
 }
 }
