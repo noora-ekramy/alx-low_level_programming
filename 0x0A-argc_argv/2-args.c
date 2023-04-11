f8993090f1738d2dfc42409348119038525c53e8
Nora Ekramy <nora.ekramy11@gmail.com>
2023-04-11 03:54:46 +0200

    Create 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints all arguments
+ *
+ * @argc: The number of arguments
+ * @argv: An array of strings
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i < argc; i++) {
+printf("%s\n", argv[i]);
+}
+return (0);
+}
