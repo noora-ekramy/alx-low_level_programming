33dea13c858a839b8f34d1e5146fedd729fa0eac
Nora Ekramy <nora.ekramy11@gmail.com>
2023-08-01 20:00:03 +0300

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,15 +9,21 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-int i, j;
+unsigned int i, j;
 
-for (i = 0; s[i] != '\0'; i++)
-{
-for (j = 0; accept[j] != '\0'; j++)
-{
-if (s[i] == accept[j])
-return (s + i);
-}
-}
-return (NULL);
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; *(accept + j); j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				break;
+			}
+		}
+		if (*(accept + j) != '\0')
+		{
+			return (s + i);
+		}
+	}
+	return (0);
 }
