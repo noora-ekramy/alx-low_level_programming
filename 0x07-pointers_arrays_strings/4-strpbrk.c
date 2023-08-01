44c04464f29e27f72038cb35eab5309c9fb4b1b8
Nora <nora.ekramy11@gmail.com>
2023-08-01 17:51:26 +0200

    Revert "Update 4-strpbrk.c"
    This reverts commit 6104e7fdc1daa862e061c2ea102b6ef6f7745862.

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,28 +1,23 @@
 #include"main.h"
 /**
- * _strpbrk - prints the consecutive caracters of s1 that are in s2.
- * @s: source string
- * @accept: searching string
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: the string to search
+ * @accept: the set of bytes to search for
  *
- * Return: new string.
+ * Return: pointer to the byte in s that matches one of the bytes in accept,
+ *         or NULL if no such byte is found
  */
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int i, j;
+int i, j;
 
-	for (i = 0; *(s + i); i++)
-	{
-		for (j = 0; *(accept + j); j++)
-		{
-			if (*(s + i) == *(accept + j))
-			{
-				break;
-			}
-		}
-		if (*(accept + j) != '\0')
-		{
-			return (s + i);
-		}
-	}
-	return (0);
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+return (s + i);
+}
+}
+return (NULL);
 }
