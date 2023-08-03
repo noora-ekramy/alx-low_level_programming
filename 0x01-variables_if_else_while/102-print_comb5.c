58186b8567bd7cafe20f85d3d8ee176229b037d2
Nora <nora.ekramy11@gmail.com>
2023-08-03 06:46:00 -0700

    fixing 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,31 +6,28 @@
  */
 int main(void)
 {
-int i, j;
+	int i, j;
 
-for (i = 0; i <= 99; i++)
-{
-for (j = i; j <= 99; j++)
-{
-int num1 = i / 10;
-int num2 = i % 10;
-int num3 = j / 10;
-int num4 = j % 10;
-if (num1 == num3 && num2 == num4)
-{
-continue;
-}
-putchar(num1 ? num1 + '0' : '0');
-putchar(num2 ? num2 + '0' : '0');
-putchar(' ');
-putchar(num3 ? num3 + '0' : '0');
-putchar(num4 ? num4 + '0' : '0');
-if (i != 99 || j != 99)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-return (0);
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i + 1; j <= 99; j++)
+		{
+			int num1 = i / 10;
+			int num2 = i % 10;
+			int num3 = j / 10;
+			int num4 = j % 10;
+			putchar(num1 + '0');
+			putchar(num2 + '0');
+			putchar(' ');
+			putchar(num3 + '0');
+			putchar(num4 + '0');
+			if (i == 98 &&  j == 99)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
 }
---
+++ 0x08-recursion/task3.c
@@ -0,0 +1,12 @@
+#include"main.h"
+int _strlen_recursion(char *s);
+{
+	int l;
+
+	for(l=0 ; s != '/0' ; l++)
+	{
+
+	}
+	return l; 
+}
+
