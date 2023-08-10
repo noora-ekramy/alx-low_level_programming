d84a6681487583a916c2347e46f2bed0247d94c1
Nora Ekramy <nora.ekramy11@gmail.com>
2023-08-11 02:00:15 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,38 +1,115 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
+/**
+ * print_error_exit - Prints "Error" and exits with status 98.
+ */
+void print_error_exit(void)
+{
+    printf("Error\n");
+    exit(98);
+}
 
 /**
- * main - multiplies nums
- * @argc: n arguments
- * @argv: args
- * Return: int
-**/
-
+ * main - Entry point. Multiplies two positive numbers.
+ * @argc: The number of command line arguments.
+ * @argv: An array containing the command line arguments.
+ * Return: 0 on success, 98 on error.
+ */
 int main(int argc, char *argv[])
 {
-	unsigned long mul;
-	int i, j;
+    if (argc != 3)
+    {
+        print_error_exit();
+    }
 
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+    char *num1_str = argv[1];
+    char *num2_str = argv[2];
 
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{
-				printf("Error\n");
-				exit(98);
-			}
-		}
-	}
-	mul = atol(argv[1]) * atol(argv[2]);
-	printf("%lu\n", mul);
-	return (0);
+    // Check if num1 and num2 are composed of digits
+    for (int i = 0; num1_str[i]; i++)
+    {
+        if (num1_str[i] < '0' || num1_str[i] > '9')
+        {
+            print_error_exit();
+        }
+    }
+
+    for (int i = 0; num2_str[i]; i++)
+    {
+        if (num2_str[i] < '0' || num2_str[i] > '9')
+        {
+            print_error_exit();
+        }
+    }
+
+    // Convert num1 and num2 to integers
+    unsigned long long num1 = strtoull(num1_str, NULL, 10);
+    unsigned long long num2 = strtoull(num2_str, NULL, 10);
+
+    // Perform multiplication
+    unsigned long long result = num1 * num2;
+
+    // Print the result
+    printf("%llu\n", result);
+
+    return 0;
 }
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_error_exit - Prints "Error" and exits with status 98.
+ */
+void print_error_exit(void)
+{
+    printf("Error\n");
+    exit(98);
+}
+
+/**
+ * main - Entry point. Multiplies two positive numbers.
+ * @argc: The number of command line arguments.
+ * @argv: An array containing the command line arguments.
+ * Return: 0 on success, 98 on error.
+ */
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        print_error_exit();
+    }
+
+    char *num1_str = argv[1];
+    char *num2_str = argv[2];
+
+    // Check if num1 and num2 are composed of digits
+    for (int i = 0; num1_str[i]; i++)
+    {
+        if (num1_str[i] < '0' || num1_str[i] > '9')
+        {
+            print_error_exit();
+        }
+    }
+
+    for (int i = 0; num2_str[i]; i++)
+    {
+        if (num2_str[i] < '0' || num2_str[i] > '9')
+        {
+            print_error_exit();
+        }
+    }
+
+    // Convert num1 and num2 to integers
+    unsigned long long num1 = strtoull(num1_str, NULL, 10);
+    unsigned long long num2 = strtoull(num2_str, NULL, 10);
+
+    // Perform multiplication
+    unsigned long long result = num1 * num2;
+
+    // Print the result
+    printf("%llu\n", result);
+
+    return 0;
+}
+
