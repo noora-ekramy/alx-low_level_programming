602f0e21205d0ee0c62ceb2d50e2a1f0b908ca81
Nora <nora.ekramy11@gmail.com>
2023-08-24 05:58:23 -0700

    list len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "list.h"
+/**
+ * list_len - returns number of list elements
+ * @h: list head
+ * Return:number of list elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *current = h;
+
+	while (current != NULL)
+	{
+		count++;
+		current = current->next;
+	}
+	return (count);
+}
+
