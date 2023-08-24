7d1d8f39f7066625ebf031919fdee22fb3020c51
Nora <nora.ekramy11@gmail.com>
2023-08-24 04:20:05 -0700

    test

---
+++ 0x12-singly_linked_lists/test/0-print_list.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * size_t print_list - ptints list elements
+ * @h: list
+ * Retur: list elements
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *current = h;
+
+	while (current != NULL)
+	{
+		if (current -> str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", current->len, current->str);
+		}
+
+		count++;
+		current = current -> next;
+	}
+	return (count);
+}
