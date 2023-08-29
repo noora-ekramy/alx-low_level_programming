aa8f6b7603625459cb375208e52d5a43628b1ded
Nora <nora.ekramy11@gmail.com>
2023-08-29 05:42:36 -0700

    task 0 done

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_listint - a function that prints
+ *  all the elements of a list
+ *  @h: list head
+ *  Return: the list size
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%u\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
