dc61aaa0eee2deb62eedae4e0fda225b55214f0a
Nora <nora.ekramy11@gmail.com>
2023-08-29 17:14:58 -0700

    task 3

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,17 +10,17 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *new_node = malloc(sizeof(listint_t));
 
-if (new_node)
-{
-new_node->n = n;
-new_node->next = *head;
-*head = new_node;
-return new_node; // Return the address of the new element
-}
-else
-{
-return NULL; // Return NULL if allocation failed
-}
+	if (new_node)
+	{
+		new_node->n = n;
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	else
+	{
+		return (NULL);
+	}
 }
