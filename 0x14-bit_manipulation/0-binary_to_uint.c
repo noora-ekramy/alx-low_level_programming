423d9026cff894702ad700d50591fddab44070fc
Nora <nora.ekramy11@gmail.com>
2023-08-31 16:17:58 -0700

    beetty fixed

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,14 +8,14 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int ans;
 	int i;
-	
+
 	if (b == NULL)
 	{
 		return (0);
 	}
 
 	ans = 0;
-	for (i=0 ; b[i] != '\0' ; i++)
+	for (i = 0 ; b[i] != '\0' ; i++)
 	{
 		if (b[i] == '0' || b[i] == '1')
 		{
