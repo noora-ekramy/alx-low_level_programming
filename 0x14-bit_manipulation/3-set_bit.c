cbc511372ff223ddbdebc2517964b19c4544457c
Nora <nora.ekramy11@gmail.com>
2023-08-31 17:51:01 -0700

    task 3 done

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#includ "mian.h"
+/**
+ * set_bit - sets the value of a bit to 1 at a given index
+ * @n: number
+ * @index: index
+ * Return: 1 if it worked, else -1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+	i = 1 << index;
+	*n = *n | i;
+
+	return (1);
+}
+
