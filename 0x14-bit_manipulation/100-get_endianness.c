ae2e6a80cea93703e9a4d896c08f13a4fed8bef1
Nora <nora.ekramy11@gmail.com>
2023-08-31 17:39:07 -0700

    task 7 done

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - check endianness
+ * Return: 0 if big endian else 1
+ */
+int get_endianness(void)
+{
+	int num;
+
+	num = 1;
+	if (*(char *)&num == 1)
+		return (1);
+	else
+		return (0);
+}
+
